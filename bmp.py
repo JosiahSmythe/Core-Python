7d1d448d2f242e885b14ec9f350aebf6524f6277
Josiah Smythe <43389776+JosiahSmythe@users.noreply.github.com>
2021-01-11 12:06:08 -0500

    Added functionality for building bmp file.

--- bmp.py
+++ bmp.py
@@ -1,6 +1,5 @@
 """A module for dealing with BMP bitmap image files."""
 
-
 def write_grayscale(filename, pixels):
     """Creates and writes a grayscale BMP file.
 
@@ -67,3 +66,43 @@ def write_grayscale(filename, pixels):
         bmp.seek(pixel_offset_bookmark)
         bmp.write(_int32_to_bytes(pixel_data_bookmark))
 
+
+def _int32_to_bytes(i):
+    "Convert an integer to four bytes in little-endian format."
+    return bytes((i & 0xff,
+                  i >> 8 & 0xff,
+                  i >> 16 & 0xff,
+                  i >> 24 & 0xff))
+
+
+def dimensions(filename):
+    """Determine the dimensions in pixels of a BMP image.
+
+    Args:
+        filename: The filename of a BMP file.
+
+    Returns:
+        A tuple containing two integers with the width
+        and height in pixels.
+
+    Raises:
+        ValueError: If the file was not a BMP file.
+        OSError: If there was a problem reading the file.
+    """
+
+    with open(filename, 'rb') as f:
+        magic = f.read(2)
+        if magic != b'BM':
+            raise ValueError("{} is not a BMP file".format(filename))
+
+        f.seek(18)
+        width_bytes = f.read(4)
+        height_bytes = f.read(4)
+
+        return (_bytes_to_int32(width_bytes),
+                _bytes_to_int32(height_bytes))
+
+
+def _bytes_to_int32(b):
+    "Convert a bytes object containing four bytes into an integer."
+    return b[0] | (b[1] << 8) | (b[2] << 16) | (b[3] << 24)
